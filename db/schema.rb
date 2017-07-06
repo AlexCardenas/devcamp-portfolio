ba590e8974cf87159e1c12d6e20bf122d79eef4f
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 17:46:40 +0000

    Implemented valdiation for blogs, skills and portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706152316) do
+ActiveRecord::Schema.define(version: 20170628153320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20170706152316) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 20170706152316) do
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
+    t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.text     "thumb_image"
   end
 
   create_table "skills", force: :cascade do |t|
